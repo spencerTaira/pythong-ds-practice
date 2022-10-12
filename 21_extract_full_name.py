6129e9c80e6944094b7e842c21433acd999a12e0
Spencer Taira <spencermtaira@gmail.com>
2022-10-12 10:14:02 -0700

    finished up to 21

--- 21_extract_full_name.py
+++ 21_extract_full_name.py
@@ -14,3 +14,7 @@ def extract_full_names(people):
         >>> extract_full_names(names)
         ['Ada Lovelace', 'Grace Hopper']
     """
+    names = []
+    for person in people:
+        names.append(f"{person['first']} {person['last']}")
+    return names
